e6b8fe60d32c7beb10d9c604fe4a5b6f6ab0e44b
LuizAlbertoDev <luizalbertodev@gmail.com>
2021-12-09 20:11:22 -0300

    Criando Funções

---
+++ Arquivos em HTML/JS-Funções/JS-FunçõesSoma.js
@@ -0,0 +1,8 @@
+
+//Caso nao seja repassado o valor do n1 ou n2, eles vao ser usados como 0 ( n1=0 )
+function soma (n1=0, n2=0) {
+    return n1 + n2
+}
+
+//2 e 5 vao ficar no lugar do N1 (2) e n2 (5)
+console.log(soma(2, 5))
---
+++ Arquivos em HTML/JS-Funções/JS-FunçõesVezes.js
@@ -0,0 +1,7 @@
+
+let v = function(x) {
+    return x*2
+}
+
+//Returna 5 * 2
+console.log(v(5))
